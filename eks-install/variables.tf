1dc8c060d7c3b93590b6410ed76dab44178f3d33
MB.Kalpana <70999645+MBkalpana@users.noreply.github.com>
2025-03-13 17:55:58 +0530

    Update variables.tf

--- eks-install/variables.tf
+++ eks-install/variables.tf
@@ -13,7 +13,7 @@ variable "vpc_cidr" {
 variable "availability_zones" {
   description = "Availability zones"
   type        = list(string)
-  default     = ["ap-south-1a, ap-south-1b, ap-south-1c"]
+  default     = ["ap-south-1a", "ap-south-1b","ap-south-1c"]
 }
 
 variable "private_subnet_cidrs" {
